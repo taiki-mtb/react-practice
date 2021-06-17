fc9ba92f31cfcd0aa52b2bc4afca619688ce792d
taiki-mtb <68456089+taiki-mtb@users.noreply.github.com>
2021-06-18 00:41:42 +0900

    6/17

--- src/index.js
+++ src/index.js
@@ -143,3 +143,74 @@
 // console.log(arr8);
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapsやfilterを使った配列の処理
+ */
+
+// const nameArr = ["田中", "山田", "たいき"];
+//  for (let index = 0; index < nameArr.length; index++) {
+//    console.log(`${index + 1}番目は${nameArr[index]}です。`);
+//  };
+
+//  const nameArr2 = nameArr.map((name)=>{
+//    return name;
+//  })
+//  console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num)=> {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "たいき") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// 条件 ? 条件がtrueの時　: 条件がfaulseの時
+// const val1 = 1 < 0 ? 'trueです'　: 'faulseです';
+// console.log(val1);
+
+// const num = 1300;
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : "許容範囲内です";
+// }
+// console.log(checkSum(50, 60));
+
+/**
+ * 論理演算子の本当の意味を知ろう && //
+ */
+// const flag1 = true;
+// const flag2 = true;
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueです")
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
